86155ffb506c638e0f8458304eb4c304ebeeae0f
Mikita Bialko <bloodyrainday@gmail.com>
2025-10-09 19:34:03 +0300

    fix: solve convertToRomanNumerals task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -128,8 +128,32 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(data) {
+  let num = data;
+  const romanNums = [
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
+  let result = '';
+  for (let i = 0; i < romanNums.length; i += 1) {
+    while (num >= romanNums[i].value) {
+      result += romanNums[i].numeral;
+      num -= romanNums[i].value;
+    }
+  }
+
+  return result;
 }
 
 /**
